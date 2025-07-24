edf96626db74dda9606531ddc24e6f9409a55bcf
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2025-07-24 21:43:42 +0200

    chore: configure environment variables for project

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SERVER_API_URL: process.env.SERVER_API_URL,
+  },
 };
 
 export default nextConfig;
